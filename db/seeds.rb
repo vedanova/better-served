0444f32c709d0ee5626d229122255144e232d769
Christoph Klocker <christoph@vedanova.com>
2014-03-10 09:52:11 +0100

    initial commit (generated with RailsBricks v2.0.4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,3 @@ u.save!
 
 
 
-# Test user accounts
-(1..5).each do |i|
-  u = User.new(
-      username: "user#{i}",
-      email: "user#{i}@example.com",
-      password: "1234",
-      password_confirmation: "1234"
-  )
-  u.skip_confirmation!
-  u.save!
-
-  puts "#{i} test users created..." if (i % 5 == 0)
-
-end
-  
-
