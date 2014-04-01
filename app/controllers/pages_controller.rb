3cb8f1096fd615786702bc1c705a796f66898f7e
Christoph Klocker <christoph@vedanova.com>
2014-04-01 14:34:38 +0200

    fixed redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,11 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:inside]
 
   def home
-    redirect_to new_user_session_path
+    if current_user
+      redirect_to dashboard_index_path
+    else
+      redirect_to new_user_session_path
+    end
   end
   
   def inside
